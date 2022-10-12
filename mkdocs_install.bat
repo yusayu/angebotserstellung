dc476bc10b31800d23534c89b4d1a5d4575d9480
Hiekel <i21hiekelto>
2022-10-12 12:02:40 +0200

    proxy file changed to include git global config for proxy

--- mkdocs_install.bat
+++ mkdocs_install.bat
@@ -1,1 +1,2 @@
-pip --proxy http://kjs-03.lan.dd-schulen.de:3128  install mkdocs mkdocs-material
+pip --proxy http://kjs-03.lan.dd-schulen.de:3128 install mkdocs mkdocs-material
+git --config global http.proxy http://kjs-03.lan.dd-schulen.de:3128
