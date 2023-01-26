215799781361b90761db92c6fe26f62b62dca516
Hiekel <i21hiekelto>
2023-01-26 07:39:14 +0100

    update

--- to_git.bat
+++ to_git.bat
@@ -1,6 +1,7 @@
 git add .
 git commit
-git push https://github.com/mehmetalici/angebotserstellung
+::git push https://github.com/mehmetalici/angebotserstellung
+git push https://github.com/yusayu/angebotserstellung
 ::%APPDATA%\Roaming\Python\Python310\Scripts\mkdocs.exe gh-deploy
 ::"C:\\Program Files\\Python3\\Scripts\mkdocs.exe" gh-deploy
 C:\Users\i21hiekelto\AppData\Roaming\Python\Python310\Scripts\mkdocs.exe gh-deploy
